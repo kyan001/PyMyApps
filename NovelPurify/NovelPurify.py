6624906270afb933e56a838a1f94730624b9fe41
Kyan <ykyankai@gmail.com>
2016-05-18 17:50:15 +0800

    update novelpurify

--- NovelPurify/NovelPurify.py
+++ NovelPurify/NovelPurify.py
@@ -1,5 +1,5 @@
 import sys, os, re, string
-import tkinter.filedialog
+import tkinter, tkinter.filedialog
 sys.path.append('../')
 import KyanToolKit_Py
 ktk = KyanToolKit_Py.KyanToolKit_Py()
@@ -56,12 +56,18 @@ def hanziPurify(txt):
         return l
 
     def matchInTable(table, result_list=[]):
+        '判断 txt 里面 match 多少个 table 中的拼音'
         ktk.pStart()
         if not table:
             raise Exception('table 不能为空')
         for hanzi, pinyin in table.items():
-            pinyin_list = generateToneList(pinyin)
-            pattern = re.compile('[^a-zA-Z]( *(?:'+'|'.join(pinyin_list)+') *)[^a-zA-Z]', flags=re.IGNORECASE)
+            pinyin_list = []
+            if type(pinyin) == tuple: # '欲': (yu, yv)
+                for py in pinyin:
+                    pinyin_list += generateToneList(py)
+            else: # '好': 'hao'
+                pinyin_list += generateToneList(pinyin)
+            pattern = re.compile(r'[^a-zA-Z]( *(?:{}) *)[^a-zA-Z]'.format('|'.join(pinyin_list)), flags=re.IGNORECASE)
             matches = pattern.findall(txt)
             if len(matches):
                 result_list.append([hanzi, pattern])
@@ -91,54 +97,96 @@ def hanziPurify(txt):
         '荡': 'dàng',
         '毛': 'máo',
         '精': 'jīng',
+        '花': 'huā',
+        '洞': 'dòng',
+        '性': 'xìng',
+        '床': 'chuáng',
+        '阴': 'yīn',
+        '激': 'jī',
+        '交': 'jiāo',
+        '欲': ('yǜ', 'yù'),
+        '药': 'yào',
+        '浪': 'làng',
+        '迷': 'mí',
+        '肥': 'féi',
+        '揉': 'róu',
+        '妇': 'fù',
+        '抽': 'chōu',
+        '弄': 'nòng',
+        '逼': 'bī',
+        '私': 'sī',
+        '波': 'bō',
+        '魂': 'hun',
+        '腿': 'tuǐ',
+        '挺': 'tǐng',
     }
+
     word_table = {
         '十有八九': '十有**',
-        '兵痞子': '兵**',
         '侍女': 'shìnv',
-        '世界': 'shìjiè',
         '自己': 'zìjǐ',
-        '什么': 'shíme',
+        '什么': ('shénmè', 'shímè'),
         '没有': 'méiyoǔ',
         '清楚': 'qīngchǔ',
         '那么': 'nàme',
-        '周围': 'zhōuwéi',
         '已经': 'yǐjīng',
         '时候': 'shíhòu',
         '时间': 'shíjiān',
+        '裸露': 'luǒlù',
+        '自由': 'zìyoú',
     }
     matched_patterns = []
+    ktk.info('查找并替换词组')
     matched_patterns = matchInTable(word_table, matched_patterns)
+    ktk.info('查找并替换单字')
     matched_patterns = matchInTable(hanzi_table, matched_patterns)
     if matched_patterns:
         ktk.pStart().echo('确认将「拼音」替换为以上「汉字」吗？', prefix='问题')
         answer = ktk.getChoice(['是', '否'])
         if '是' == answer:
-            for hanzi, pattern in matched_patterns:
-                ktk.info('Replacing {}'.format(hanzi))
+            for i, (hanzi, pattern) in enumerate(matched_patterns):
+                total = len(matched_patterns)
+                ktk.echo('Replacing {}'.format(hanzi), prefix="{}/{total}".format(i+1, total=total))
                 txt = pattern.sub(hanzi, txt)
             ktk.info('完成').pEnd()
     return txt
 
-def unmatchedWordPurify(txt):
-    ktk.pStart().pTitle('未匹配的英文字符串')
-    tone_chars = 'āáǎàēéěèīíǐìōóǒòūúǔùǖǘǚǜ'
-    word_pattern = re.compile('[^a-zA-Z]( *[a-zA-Z.{}]+ *)[^a-zA-Z]'.format(tone_chars), flags=re.IGNORECASE)
-    txt = findCheckDelete(word_pattern, txt, undelete=True)
-    ktk.pEnd()
-    return txt
-
 def unmatchedUrlPurify(txt):
     ktk.pStart().pTitle('未匹配的 url')
-    url_pattern = re.compile('([a-zA-Z.:/ ]*(?:www|http|com|org|net)[a-zA-Z0-9 \t{pncttn}]*)'.format(pncttn=string.punctuation), flags=re.IGNORECASE)
+    chars_in_url = r"a-zA-Z0-9 \!\"\#\$\%\&\'\(\)\*\+\,\-\.\/\:\;\<\=\>\?\@\[\]\^\`\{\|\}\~\\"
+    url_pattern = re.compile(r'[{0}]*(?:www|http|com|org|net|span|div)[{0}]*'.format(chars_in_url), flags=re.IGNORECASE)
     txt = findCheckDelete(url_pattern, txt)
     ktk.pEnd()
     return txt
 
-def unmatchedUrlBlockPurify(txt):
-    ktk.pStart().pTitle('未匹配的 url 段落')
-    url_block_pattern_www = re.compile('[\n]([^\r\n]*(?:www|http|com|org|net)[^\r\n]*)[\n]', flags=re.IGNORECASE)
-    txt = findCheckDelete(url_block_pattern_www, txt)
+def showParagraph(txt):
+    ktk.pStart().pTitle('优化格式不正确的章节头')
+    wrong_title_pattern = re.compile(r'([\S])(第[一二三四五六七八九十百 ]+[卷章回节])([\S])', flags=re.IGNORECASE)
+    matches = wrong_title_pattern.findall(txt)
+    wrong_titles = set(matches)
+    if wrong_titles:
+        ktk.info('共发现 {} 个格式错误的章节头'.format(len(wrong_titles)))
+        right_title_pattern = r'\1 \2 \3'
+        txt = wrong_title_pattern.sub(right_title_pattern, txt)
+        ktk.info('优化完成').pEnd()
+    ktk.pEnd()
+    return txt
+
+def showUnmatchedWord(txt):
+    '显示尚未匹配到的英文字符串'
+    ktk.pStart().pTitle('未匹配的英文字符串：TOP {}'.format(threshold))
+    threshold = 10;
+    tone_chars = 'āáǎàēéěèīíǐìōóǒòūúǔùǖǘǚǜ'
+    word_pattern = re.compile(r'[^a-zA-Z]( *[a-zA-Z{0}][a-zA-Z.{0}]+ *)[^a-zA-Z]'.format(tone_chars), flags=re.IGNORECASE)
+    matches = word_pattern.findall(txt)
+    unmatched_words = list(set(matches))
+    if unmatched_words:
+        unm_word_dict = {}
+        for uw in unmatched_words:
+            unm_word_dict[uw] = matches.count(uw)
+    uw_tops = sorted(unm_word_dict.items(), key=lambda Ditem:Ditem[1], reverse=True)[:threshold]
+    for uw, count in uw_tops:
+        ktk.info('"{}"：{}'.format(uw, count))
     ktk.pEnd()
     return txt
 
@@ -153,14 +201,23 @@ def save(filepath, txt):
 
 def main():
     filepath = sys.argv[1] if len(sys.argv) > 1 else None
-    filepath = "jiangye.txt"
     if not filepath:
+        tkapp = tkinter.Tk()
         filepath = tkinter.filedialog.askopenfilename()
-    with open(filepath, mode='r', encoding="utf-8") as f:
-        txt = f.read()
-    txt = hanziPurify(txt)
-    txt = unmatchedUrlPurify(txt)
-    txt = unmatchedUrlBlockPurify(txt)
-    txt = unmatchedWordPurify(txt)
+        tkapp.destroy()
+    try:
+        with open(filepath, mode='r', encoding="utf-8") as f:
+            txt = f.read()
+            ktk.info('以 utf-8 格式打开文件')
+    except UnicodeDecodeError:
+        with open(filepath, mode='r', encoding='gbk') as f:
+            txt = f.read()
+            ktk.info('以 gbk 格式打开文件')
+    #txt = hanziPurify(txt)
+    #txt = unmatchedUrlPurify(txt)
+    #txt = showUnmatchedWord(txt)
+    txt = showParagraph(txt)
     save(filepath, txt)
-main()
+
+if __name__ == '__main__':
+    main()
