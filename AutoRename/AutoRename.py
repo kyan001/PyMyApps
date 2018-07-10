86e99a59ee222a5058c2654a84c019fc584ec316
Kyan <ykyankai@gmail.com>
2018-07-10 22:39:46 +0800

    use argparse to deal with args

--- AutoRename/AutoRename.py
+++ AutoRename/AutoRename.py
@@ -4,56 +4,60 @@ import os
 import re
 import tkinter
 import tkinter.filedialog
+import argparse
+
 import consoleiotools as cit
 
 
-__version__ = '1.0.2'
+__version__ = '1.1.2'
+__prog__ = "AutoRename"
+__description__ = "Auto rename files in a folder"
+__epilog__ = "TL;DR: Run program with no args, or drag & drop a folder on it."
 
 # Global Variables
 _PATTERN = r'S[0-9][0-9]E[0-9][0-9]'
 
 
 @cit.as_session
-def get_file_dir():
+def get_file_dir(filedir=os.getcwd()):
     """Get file dir path
+
+    Args:
+        filedir: str.
 
     Returns:
         filedir: string of files' dir
     """
-    if len(sys.argv) > 1:  # if drag & drop folder on .py file
-        filedir = sys.argv[1]
-    else:  # .py file is in the files' folder or manually set
-        filedir = os.getcwd()
-        cit.ask('Set file dir to "{}"?'.format(filedir))
-        how_to_locate = cit.get_choice(['Yes', 'No, let me select', 'No, let me type'])
-        if how_to_locate == 'No, let me select':  # manually select
-            tkapp = tkinter.Tk()
-            filedir = tkinter.filedialog.askdirectory()
-            tkapp.destroy()
-        elif how_to_locate == 'No, let me type':  # manually type
-            filedir = cit.get_input('Please enter your dir:').strip('"').strip("'")
+    cit.ask('Set file dir to "{}"?'.format(filedir))
+    how_to_locate = cit.get_choice(['Yes', 'No, let me select', 'No, let me type'])
+    if how_to_locate == 'No, let me select':  # manually select
+        tkapp = tkinter.Tk()
+        filedir = tkinter.filedialog.askdirectory()
+        tkapp.destroy()
+    elif how_to_locate == 'No, let me type':  # manually type
+        filedir = cit.get_input('Please enter your dir:').strip('"').strip("'")
     cit.info('File dir: "{}"'.format(filedir))
     return filedir
 
 
 @cit.as_session
-def get_file_list(filedir: str):
+def get_files(filedir: str):
     """Get all the file names in the path
 
     Args:
         filedir: the folder path of the files' dir
 
     Returns:
-        filelist: list of files in filedir
+        files: list of files in filedir
     """
     if os.path.isdir(filedir):
-        filelist = os.listdir(filedir)
+        files = os.listdir(filedir)
     else:
         cit.err('"{}" is not a path'.format(filedir)).bye()
     cit.info('File list:')
-    for fname in filelist:
+    for fname in files:
         cit.echo(fname)
-    return filelist
+    return files
 
 
 @cit.as_session
@@ -78,19 +82,19 @@ def get_keyword(filedir: str):
 
 
 @cit.as_session('Get Name Mapping')
-def get_name_map(pattern: str, filelist: list, keyword: str):
+def get_name_map(pattern: str, files: list, keyword: str):
     """Get name mapping, old name as key, new name as value
 
     Args:
         pattern: pattern to test
-        filelist: list
+        files: list
         keyword: str
 
     Returns:
         namemap: dict
     """
     # validations
-    if not filelist:
+    if not files:
         cit.err('No file list').bye()
     if not pattern:
         cit.warn('No pattern detected, use default pattern.')
@@ -98,7 +102,7 @@ def get_name_map(pattern: str, filelist: list, keyword: str):
     cit.info('Testing pattern: `{}`'.format(pattern))
     # generate namemap
     namemap = {}
-    for fname in filelist:
+    for fname in files:
         mtch = re.compile(pattern).findall(fname)
         if not mtch:
             cit.warn('Ignored: {}'.format(fname))
@@ -116,7 +120,7 @@ def get_name_map(pattern: str, filelist: list, keyword: str):
     else:
         cit.warn('Pattern does not match any file!')
     new_pattern = cit.get_input("Please enter a new pattern: `[0-9] [a-Z] . * + ()`")
-    return get_name_map(new_pattern, filelist, keyword)
+    return get_name_map(new_pattern, files, keyword)
 
 
 def generate_cmds(filedir: str, namemap: dict):
@@ -138,11 +142,19 @@ def generate_cmds(filedir: str, namemap: dict):
 
 
 if __name__ == '__main__':
+    # Args
+    parser = argparse.ArgumentParser(prog=__prog__, description=__description__, epilog=__epilog__)
+    parser.add_argument("-v", "--version", action="version", version=__version__)
+    parser.add_argument(dest="filedir", metavar="FILEDIR", nargs="?", default=os.getcwd(), help="Folder path of target files.")
+    parser.add_argument("-k", "--keyword", dest="keyword", help="Prefix of new filename.")
+    parser.add_argument("-p", "--pattern", dest="pattern", default=_PATTERN, help="REGEX (REGular EXpression) which matches a part of filenames.")
+    parser.add_argument("-f", "--file", dest="files", action="append", nargs="*", help="target files in filedir, repeat it to add more files.")
+    args = parser.parse_args()
     # get Vars
-    filedir = get_file_dir()
-    filelist = get_file_list(filedir)
-    keyword = get_keyword(filedir)
-    namemap = get_name_map(pattern=_PATTERN, filelist=filelist, keyword=keyword)
+    filedir = get_file_dir(args.filedir)
+    files = args.files or get_files(filedir)
+    keyword = args.keyword or get_keyword(filedir)
+    namemap = get_name_map(pattern=args.pattern, files=files, keyword=keyword)
     # get cmd
     cmds = generate_cmds(filedir=filedir, namemap=namemap)
     cit.warn('Commands example for final comfirm:')
