4ae8b1aa2e1d5d3b0eb74fd5ec5b064ea0a81b3f
Kyan <ykyankai@gmail.com>
2019-11-15 07:31:31 +0800

    init ProxyConf

---
+++ ProxyConf/update_confs.py
@@ -0,0 +1,12 @@
+import KyanToolKit as ktk
+import consoleiotools as cit
+
+def replaceFinal(file_, mode="DIRECT"):
+
+
+def main():
+    ktk.updateFile("./shdwrckt_gfwlst.conf", "https://raw.githubusercontent.com/XinSSS/Conf-for-Surge-Shadowrocket/master/configFileHere/shadowrocket_gfwlist%26whiteIP.conf")
+
+
+if __name__ == "__main__":
+    main()
