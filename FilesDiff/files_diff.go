f170f5c71d5bd634ad3c9a5bd1d4c84be1a41e70
Kyan <ykyankai@gmail.com>
2021-02-02 13:17:00 +0800

    update to 1.5.1

--- FilesDiff/files_diff.go
+++ FilesDiff/files_diff.go
@@ -11,13 +11,20 @@ import (
 	"strings"
 	"hash/crc32"
 	"github.com/sbwhitecap/tqdm"
-	. "github.com/sbwhitecap/tqdm/iterators"
+	"github.com/sbwhitecap/tqdm/iterators"
 )
 
-const LISTFILE_PREFIX = "songlist-"
-const LISTFILE_SUFFIX = ".json"
-const SUFFIX_HOSTNAME bool = true
+const version = "1.5.1"
+
+var BASE_DIR, dir_err = os.Getwd()
+var HOSTNAME, host_err = os.Hostname()
+
 const TARGET_FILE_PATTERN = ".mp3"
+var LISTFILE_PREFIX = TARGET_FILE_PATTERN[1:] + "list-"
+const LISTFILE_SUFFIX = ".json"
+const HASH_MODE = "CRC32"  // "CRC32", "MTIME"
+const SEPARATE_HOST bool = true
+
 
 func LogErr(err error) {
 	if err != nil {
@@ -25,39 +32,49 @@ func LogErr(err error) {
 	}
 }
 
+
 func Info(texts ...string) {
-	fmt.Println("| [Info]", strings.Join(texts, " "))
-}
-func Warn(texts ...string) {
-	fmt.Println("| [Warn]", strings.Join(texts, " "))
-}
-func Pause() {
-	fmt.Print("Press enter to continue ...")
-	var in string
-	fmt.Scanln(&in)
+	fmt.Println("| (Info)", strings.Join(texts, " "))
 }
 
-func get_old_list_files(base_dir string) []string {
+
+func Warn(texts ...string) {
+	fmt.Println("| (Warn)", strings.Join(texts, " "))
+}
+
+
+func Pause() {
+	fmt.Print("Press Enter to Continue...")
+	var press_enter string
+	fmt.Scanln(&press_enter)
+}
+
+func get_old_list_files() []string {
 	var list_files []string
-	files, err := ioutil.ReadDir(base_dir)
+	files, err := ioutil.ReadDir(BASE_DIR)
 	LogErr(err)
 	for _, f := range files {
 		if (strings.HasPrefix(f.Name(), LISTFILE_PREFIX) && filepath.Ext(f.Name()) == LISTFILE_SUFFIX) {
+			if (SEPARATE_HOST && !strings.Contains(f.Name(), HOSTNAME)) {
+				continue
+			}
 			list_files = append(list_files, f.Name())
 		}
 	}
 	return list_files
 }
 
-func read_old_list_file(base_dir string) map[string]uint32 {
+
+func read_old_list_file() map[string]uint32 {
 	fmt.Println("* __ READ OLD LIST FILES ______")
 	defer fmt.Println("`")
 	var old_list_file string
 	if len(os.Args) > 1 {
-		Info("Input file:", os.Args[1])
-		old_list_file = filepath.Join(base_dir, os.Args[1])
+		input_file := filepath.Base(os.Args[1])
+		old_list_file = filepath.Join(BASE_DIR, input_file)
+		Info("Input file:", input_file)
 	} else {
-		old_list_files := get_old_list_files(base_dir)
+		old_list_files := get_old_list_files()
 		if len(old_list_files) > 0 {
 			old_list_file = old_list_files[len(old_list_files)-1]
 		} else {
@@ -79,42 +96,60 @@ func read_old_list_file(base_dir string) map[string]uint32 {
 	return nil
 }
 
-func generate_new_list(base_dir string) map[string]uint32 {
+
+func generate_new_list() map[string]uint32 {
 	fmt.Println("* __ GENERATE NEW LIST ______")
 	defer fmt.Println("`")
-	now := time.Now().Format("20060102_150405")
 	file_hashes := make(map[string]uint32)
 	Info("Target files pattern:", TARGET_FILE_PATTERN)
 	var pathlist []string
-	err := filepath.Walk(base_dir, func(fpath string, finfo os.FileInfo, err error) error {
+	err := filepath.Walk(BASE_DIR, func(fpath string, finfo os.FileInfo, err error) error {
 		if filepath.Ext(fpath) == TARGET_FILE_PATTERN {
 			pathlist = append(pathlist, fpath)
 		}
 		return nil
 	})
 	LogErr(err)
-	tqdm.With(Strings(pathlist), "", func(val interface{}) (brk bool) {
+	tqdm.With(iterators.Strings(pathlist), "", func(val interface{}) (brk bool) {
 		fpath := val.(string)
 		fbytes, err := ioutil.ReadFile(fpath)
 		LogErr(err)
-		file_hashes[filepath.Base(fpath)] = crc32.ChecksumIEEE(fbytes)
+		var fhash uint32
+		if HASH_MODE == "CRC32" {
+			fhash = crc32.ChecksumIEEE(fbytes)
+		} else if HASH_MODE == "MTIME" {
+			f, ferr := os.Open(fpath)
+			LogErr(ferr)
+			fi, fierr := f.Stat()
+			LogErr(fierr)
+			fhash = uint32(fi.ModTime().Unix())
+		} else {
+			fhash = 0
+		}
+		file_hashes[filepath.Base(fpath)] = fhash
 		return
 	})
+	return file_hashes
+}
+
+
+func save_new_list(new_list map[string]uint32) {
+	fmt.Println("* __ SAVE NEW LIST ______")
+	defer fmt.Println("`")
+	now := time.Now().Format("20060102_150405")
 	hostname_badge := ""
-	if SUFFIX_HOSTNAME {
-		hostname, err := os.Hostname()
-		LogErr(err)
-		hostname_badge = "-" + strings.ReplaceAll(hostname, "-", "")
+	if SEPARATE_HOST {
+		hostname_badge = "-" + HOSTNAME
 	}
 	new_list_filename := LISTFILE_PREFIX + now + hostname_badge + LISTFILE_SUFFIX
-	fpath := filepath.Join(base_dir, new_list_filename)
-	json_bytes, json_err := json.MarshalIndent(file_hashes, "", "    ")
+	fpath := filepath.Join(BASE_DIR, new_list_filename)
+	json_bytes, json_err := json.MarshalIndent(new_list, "", "    ")
 	LogErr(json_err)
 	write_err := ioutil.WriteFile(fpath, json_bytes, 0644)
 	LogErr(write_err)
 	Info("New list file generated:", new_list_filename)
-	return file_hashes
 }
+
 
 func dict_diffs(dict1 map[string]uint32, dict2 map[string]uint32) map[string]uint32 {
 	diffs := make(map[string]uint32)
@@ -139,11 +174,15 @@ func dict_diffs(dict1 map[string]uint32, dict2 map[string]uint32) map[string]uin
 	return diffs
 }
 
+
 func main() {
-	BASE_DIR, _ := os.Getwd()
+	Info("Version:", version)
+	LogErr(dir_err)
 	Info("BASE_DIR:", BASE_DIR)
-	old_list := read_old_list_file(BASE_DIR)
-	new_list := generate_new_list(BASE_DIR)
+	LogErr(host_err)
+	Info("HOSTNAME:", HOSTNAME)
+	old_list := read_old_list_file()
+	new_list := generate_new_list()
 	if len(old_list) > 0 && len(new_list) > 0 {
 		diffs := dict_diffs(old_list, new_list)
 		if len(diffs) > 0 {
@@ -151,10 +190,12 @@ func main() {
 			for name, _ := range diffs {
 				fmt.Println(name)
 			}
+			save_new_list(new_list)
 		} else {
 			Info("No changes")
 		}
 	} else {
+		save_new_list(new_list)
 		Info("Done")
 	}
 	Pause()
--- FilesDiff/files_diff.py
+++ FilesDiff/files_diff.py
@@ -9,21 +9,24 @@ import tqdm
 import consolecmdtools as cct
 import consoleiotools as cit
 
-__version__ = '1.2.1'
+__version__ = '1.5.1'
 
 BASE_DIR = cct.get_dir(__file__)
+HOSTNAME = socket.gethostname()
 
-LISTFILE_PREFIX = "list-"
-LISTFILE_SUFFIX = ".json"
-SUFFIX_HOSTNAME = True
 TARGET_FILE_PATTERN = "*.mp3"
+LISTFILE_PREFIX = TARGET_FILE_PATTERN.split(".")[-1] + "list-"
+LISTFILE_SUFFIX = ".json"
 HASH_MODE = "CRC32"  # "CRC32", "MD5", "NAME", "PATH", "MTIME"
+SEPARATE_HOST = True
 
 
-def get_old_list_files(dir: str) -> list:
+def get_old_list_files() -> list:
     list_files = []
-    for filename in os.listdir(dir):
+    for filename in os.listdir(BASE_DIR):
         if filename.startswith(LISTFILE_PREFIX) and filename.endswith(LISTFILE_SUFFIX):
+            if SEPARATE_HOST and (HOSTNAME not in filename):
+                continue
             list_files.append(filename)
     return list_files
 
@@ -31,9 +34,11 @@ def get_old_list_files(dir: str) -> list:
 @cit.as_session
 def read_old_list_file():
     if len(sys.argv) > 1:
-        old_list_file = os.path.join(BASE_DIR, sys.argv[1])
+        input_file = os.path.basename(sys.argv[1])
+        old_list_file = os.path.join(BASE_DIR, input_file)
+        cit.info(f"Input file: {input_file}")
     else:
-        old_list_files = get_old_list_files(BASE_DIR)
+        old_list_files = get_old_list_files()
         old_list_file = old_list_files[-1] if old_list_files else None
     if not old_list_file:
         cit.warn("Old list file does not found, ignored.")
@@ -49,11 +54,10 @@ def read_old_list_file():
 
 
 @cit.as_session
-def generate_new_list(dir):
-    now = datetime.datetime.now().strftime('%Y%m%d_%H%M%S')
+def generate_new_list():
     file_hashes = {}
     cit.info(f"Target files pattern: {TARGET_FILE_PATTERN}")
-    pathlist = list(pathlib.Path(dir).rglob(TARGET_FILE_PATTERN))
+    pathlist = list(pathlib.Path(BASE_DIR).rglob(TARGET_FILE_PATTERN))
     for fpath in tqdm.tqdm(pathlist, total=len(pathlist), unit=" files"):
         if HASH_MODE == "CRC32":
             fhash = cct.crc32(fpath)
@@ -68,12 +72,18 @@ def generate_new_list(dir):
         else:
             fhash = None
         file_hashes[os.path.basename(fpath)] = fhash
-    hostname_badge = f"-{socket.gethostname().replace('-', '')}" if SUFFIX_HOSTNAME else ""
-    new_list_filename = f"{LISTFILE_PREFIX}{now}{hostname_badge}{LISTFILE_SUFFIX}"
-    with open(new_list_filename, 'w', encoding="UTF8") as f:
-        f.write(json.dumps(file_hashes, indent=4, ensure_ascii=False))
-        cit.info(f"New list file generated: {new_list_filename}")
     return file_hashes
+
+
+@cit.as_session
+def save_new_list(new_list: dict):
+    now = datetime.datetime.now().strftime('%Y%m%d_%H%M%S')
+    hostname_badge = f"-{HOSTNAME}" if SEPARATE_HOST else ""
+    new_list_filename = LISTFILE_PREFIX + now + hostname_badge + LISTFILE_SUFFIX
+    with open(new_list_filename, 'w', encoding="UTF8") as f:
+        f.write(json.dumps(new_list, indent=4, ensure_ascii=False))
+        cit.info(f"New list file generated: {new_list_filename}")
+    clean_up_old_list_files()
 
 
 def dict_diffs(dict1, dict2):
@@ -82,7 +92,7 @@ def dict_diffs(dict1, dict2):
 
 @cit.as_session
 def clean_up_old_list_files():
-    list_files = get_old_list_files(BASE_DIR)
+    list_files = get_old_list_files()
     if len(list_files) > 1:
         old_list_files = list_files[:-1]
         cit.ask("Clean up old list files?")
@@ -97,19 +107,23 @@ def clean_up_old_list_files():
 
 
 def main():
+    cit.info(f"Version: {__version__}")
+    cit.info(f"BASE_DIR: {BASE_DIR}")
+    cit.info(f"HOSTNAME: {HOSTNAME}")
     old_list = read_old_list_file()
-    new_list = generate_new_list(BASE_DIR)
+    new_list = generate_new_list()
     if old_list and new_list:
         diffs = dict_diffs(old_list, new_list)
         if diffs:
             cit.info("Changes since last time:")
             for filename, hash in diffs:
                 cit.echo(filename)
+            save_new_list(new_list)
         else:
             cit.info("No changes")
     else:
         cit.info("Done")
-    clean_up_old_list_files()
+        save_new_list(new_list)
     cit.pause()
 
 
