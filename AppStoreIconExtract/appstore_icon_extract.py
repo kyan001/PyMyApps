358282e220f33dc7deec11edf5d83b60527704ff
Kyan <ykyankai@gmail.com>
2022-11-09 01:54:51 +0800

    add pyperclip support

--- AppStoreIconExtract/appstore_icon_extract.py
+++ AppStoreIconExtract/appstore_icon_extract.py
@@ -4,11 +4,20 @@ import webbrowser
 import consoleiotools as cit
 from bs4 import BeautifulSoup
 import requests
+try:
+    import pyperclip
+    clipboard = pyperclip.paste()
+    IS_CLIPBOARD = clipboard and clipboard.startswith("https://apps.apple.com/")
+except ImportError:
+    IS_CLIPBOARD = False
 
 
 def get_url():
     if len(sys.argv) > 1:
         return sys.argv[1]
+    if IS_CLIPBOARD:
+        cit.info("Loaded URL from clipboard.")
+        return clipboard
     return cit.get_input("Enter an App Store Detail URL:")
 
 
@@ -40,20 +49,30 @@ def get_icon_url(srcset: str) -> str:
     return widest[0]
 
 
+@cit.as_session("Extraction")
+def extraction_result(icon_url: str):
+    if not icon_url:
+        cit.err("No icon url found.")
+        cit.bye()
+    else:
+        cit.info("Icon URL:")
+        cit.br()
+        print(icon_url)
+        cit.br()
+        if not IS_CLIPBOARD:
+            cit.get_input("Open in Browser?", "> Yes")
+        webbrowser.open_new_tab(icon_url)
+
+
 def main():
     url = get_url()
     html = get_url_html(url)
     srcset = get_icon_srcset(html)
     icon_url = get_icon_url(srcset)
-    if not icon_url:
-        cit.err("No icon url found.")
-        cit.bye()
-    else:
-        cit.info(f"Icon URL: {icon_url}")
-        cit.get_input("Open in Browser?", "> Yes")
-        webbrowser.open_new_tab(icon_url)
+    extraction_result(icon_url)
 
 
 if __name__ == "__main__":
     main()
-    cit.pause()
+    if not IS_CLIPBOARD:
+         cit.pause()
